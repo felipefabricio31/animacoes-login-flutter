45859ce7ca1f1789c44a775cadc7c73e5fecfdab
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-26 14:24:29 -0300

    Finalizando as animações no login.

--- lib/widgets/stagger_animation.dart
+++ lib/widgets/stagger_animation.dart
@@ -3,34 +3,52 @@ import 'package:flutter/material.dart';
 class StaggerAnimation extends StatelessWidget {
   final AnimationController controller;
   StaggerAnimation({this.controller})
-      : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(
+      : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(CurvedAnimation(
+          parent: controller,
+          curve: Interval(0.0, 0.150),
+        )),
+        buttonZoomOut = Tween(begin: 60.0, end: 1000.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(0.0, 0.150),
+            curve: Interval(0.5, 1, curve: Curves.bounceOut),
           ),
         );
 
   final Animation<double> buttonSqueeze;
+  final Animation<double> buttonZoomOut;
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
-        padding: EdgeInsets.only(bottom: 50),
-        child: InkWell(
-          onTap: () {
-            controller.forward();
-          },
-          child: Container(
-              width: buttonSqueeze.value,
-              height: 60.0,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(247, 64, 106, 1.0),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30.0),
+      padding: EdgeInsets.only(bottom: 50),
+      child: InkWell(
+        onTap: () {
+          controller.forward();
+        },
+        child: buttonZoomOut.value <= 70
+            ? Container(
+                width: buttonSqueeze.value,
+                height: 60.0,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(247, 64, 106, 1.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                child: _buildInside(context),
+              )
+            : Container(
+                width: buttonZoomOut.value,
+                height: buttonZoomOut.value,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(247, 64, 106, 1.0),
+                  shape: buttonZoomOut.value < 500
+                      ? BoxShape.circle
+                      : BoxShape.rectangle,
                 ),
               ),
-              child: _buildInside(context)),
-        ));
+      ),
+    );
   }
 
   Widget _buildInside(context) {
